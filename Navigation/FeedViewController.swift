b95db6206a8d5188095541d92da31883b3e84e10
zilnur <86298777+zilnur@users.noreply.github.com>
2021-12-03 12:55:30 +0300

    HW10

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -92,8 +92,8 @@ final class FeedViewController: UIViewController {
     }
     
     @objc func toPostVC() {
-        let sb = storyboard?.instantiateViewController(identifier: "postVC")
-        navigationController?.pushViewController(sb!, animated: true)
+        let psotVC = PostViewController()
+        navigationController?.pushViewController(psotVC, animated: true)
     }
 }
 
