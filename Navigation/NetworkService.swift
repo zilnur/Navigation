bc898143b7a349ba992b36a9916889b363188aba
zilnur <86298777+zilnur@users.noreply.github.com>
2022-02-10 20:51:13 +0300

    HW2

--- Navigation/NetworkService.swift
+++ Navigation/NetworkService.swift
@@ -7,47 +7,89 @@
 //
 
 import Foundation
+import UIKit
+
+struct Planet: Codable {
+    let name: String
+    let rotationPeriod: String
+    let orbitalPeriod: String
+    let diameter: String
+    let climate: String
+    let gravity: String
+    let terrain: String
+    let surfaceWater: String
+    let population: String
+    let residents: [String]
+    let films: [String]
+    let created: String
+    let edited: String
+    let url: String
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case rotationPeriod = "rotation_period"
+        case orbitalPeriod = "orbital_period"
+        case diameter
+        case climate
+        case gravity
+        case terrain
+        case surfaceWater = "surface_water"
+        case population
+        case residents
+        case films
+        case created
+        case edited
+        case url
+    }
+}
 
 struct NetworkService {
     
-    func startURLSession(address: String) {
+    func startURLSession(label: UILabel) {
         
-        if let url = URL(string: address) {
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/todos/7") {
             
             let req = URLRequest(url: url)
             
             let session = URLSession.shared.dataTask(with: req) { data, response, error in
                 if let uData = data {
-                    if let answer = String(data: uData, encoding: .utf8) {
-                        print("data is\(answer)")
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: uData, options: [])
+                        
+                        if let dict = json as? [String:Any],
+                           let text = dict["title"] as? String {
+                            DispatchQueue.main.async {
+                                label.text = text
+                            }
+                        }
+                    } catch let error {
+                        print(error.localizedDescription)
                     }
                 }
-                if let uResponse = response as? HTTPURLResponse {
-                    print("response is \(uResponse.statusCode) & \(uResponse.allHeaderFields)")
+            }
+            session.resume()
+        }
+    }
+    
+    func decode(label: UILabel) {
+        
+        if let url = URL(string: "https://swapi.dev/api/planets/1") {
+            
+            let session = URLSession.shared.dataTask(with: url) { data,response, error in
+                if let uData = data {
+                    do {
+                        let answer = try JSONDecoder().decode(Planet.self, from: uData)
+                        DispatchQueue.main.async {
+                            label.text = answer.name
+                        }
+                    } catch let error {
+                        print("Error is \(error)")
+                    }
                 }
-                
-                guard let uError = error else {return}
-                print("Error is \(uError.localizedDescription)")
             }
             session.resume()
         }
     }
 }
 
-enum AppConfiguration: String, CaseIterable {
-    
-    case url1 = "https://swapi.dev/api/people/8"
-    case url2 = "https://swapi.dev/api/starships/3"
-    case url3 = "https://swapi.dev/api/planets/5"
-    
-    init?(rawValue: RawValue) {
-        switch rawValue {
-        case "https://swapi.dev/api/people/8":
-            self = .url1
-        case "https://swapi.dev/api/starships/3":
-            self = .url2
-        default :
-            self = .url3
-        }
-}
-}
+
