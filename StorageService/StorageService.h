16500da45feba2db4434db10d0a127cb9627dcd3
zilnur <86298777+zilnur@users.noreply.github.com>
2022-02-19 20:42:13 +0300

    HW1

---
+++ StorageService/StorageService.h
@@ -0,0 +1,19 @@
+//
+//  StorageService.h
+//  StorageService
+//
+//  Created by Ильнур Закиров on 19.02.2022.
+//  Copyright © 2022 Artem Novichkov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StorageService.
+FOUNDATION_EXPORT double StorageServiceVersionNumber;
+
+//! Project version string for StorageService.
+FOUNDATION_EXPORT const unsigned char StorageServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StorageService/PublicHeader.h>
+
+
